9fbc9c00c2272ef62b54765343d4254a15c0aa9a
rebleo <rml444@nyu.edu>
2022-10-28 12:58:23 -0400

    server start

--- week08/code/theServer.js
+++ week08/code/theServer.js
@@ -1,1 +1,15 @@
 console.log("a backend console message")
+// let theInput = 800
+// console.log(theInput * 7);
+//
+const theExpress = require('express');
+// console.log(theExpress)
+// 
+const myApp = theExpress();
+const myPort = myApp.listen(12333);
+
+myApp.get('/', function (theRequest,theResponse){
+    // theResponse.send("hello worlds");
+    let theMessage = '<html><head><title>my great web app</title></head><body><h4>what a website!</h4><img src="https://media.tenor.com/R_WseIIwQ8QAAAAC/beavis-computer.gif"></body></html>'
+    theResponse.send(theMessage)
+})
